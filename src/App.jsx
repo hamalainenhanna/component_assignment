27bec0342720c52654e0c1bf80e1d3e639d5c2ae
hamalainenhanna <n3haha00@students.oamk.fi>
2024-10-19 17:40:11 +0300

    luotu opendata funktio ja router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+
+import React from 'react';
 import ComponentUI from "./componentUI";
+import OpenData from './OpenData';
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {path: '/', element: <ComponentUI/>},
+  {path: '/', element: <OpenData/>}
+]);
 
 function App() {
 
   return (
 
     <div>
-
-      <ComponentUI/>
+      
+      <RouterProvider router={router}/>
 
     </div>
   )
---
+++ src/OpenData.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+function OpenData() {
+  return (
+    <div>
+      <h2>Open Data API</h2>
+    </div>
+  );
+}
+
+export default OpenData;
+
